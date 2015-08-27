423d7e00388642233314310dc22be3f60d1a57ef
Rafael Freitas <rafaelfreitasdeveloper@gmail.com>
2015-08-27 11:03:23 -0300

    alteração teste

--- financeiro/src/main/java/br/com/javaparaweb/financeiro/util/teste.java
+++ financeiro/src/main/java/br/com/javaparaweb/financeiro/util/teste.java
@@ -1,5 +1,13 @@
 package br.com.javaparaweb.financeiro.util;
 
+import java.util.Set;
+
 public class teste {
+
+	public static void main(String[] args) {
+		System.out.print("José");
+		System.out.print("Silva Moraes");
+		
+	}
 
 }
